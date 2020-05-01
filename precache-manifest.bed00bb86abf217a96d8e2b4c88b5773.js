1ef9c864768caa02eee5d9d91643a9ef17dbfefe
hamed emad <hamadaemadeldine@gmail.comgit config user.email hamadaemadeldine@gmail.comgit config user.email hamadaemadeldine@gmail.com>
2020-05-01 06:36:47 +0200

    Updates

---
+++ precache-manifest.bed00bb86abf217a96d8e2b4c88b5773.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62b5451fbf580f636f81d97027881d36",
+    "url": "/weather-api-app/index.html"
+  },
+  {
+    "revision": "626d726737a4d31140b4",
+    "url": "/weather-api-app/static/css/main.1cd22f3d.chunk.css"
+  },
+  {
+    "revision": "85dc7cc6d31f04942955",
+    "url": "/weather-api-app/static/js/2.e44f0e21.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/weather-api-app/static/js/2.e44f0e21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "626d726737a4d31140b4",
+    "url": "/weather-api-app/static/js/main.942653e2.chunk.js"
+  },
+  {
+    "revision": "0c3978097bd806ba710f",
+    "url": "/weather-api-app/static/js/runtime-main.08142a25.js"
+  }
+]);
